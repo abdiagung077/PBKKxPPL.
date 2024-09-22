8515abee704cadb9aca6af225bc80e6966f03b8f
abdiagung077 <131676339+abdiagung077@users.noreply.github.com>
2024-09-22 16:11:44 +0700

    Add files via upload

---
+++ storage/framework/views/2046cba7062e4bf381e7f64e2135ab9d.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manajemen Jadwal Ruang dan Guru FKIP</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>FKIP Schedule</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sign up</a></li>
+                    <li><a href="#">Log in</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    <section class="landing">
+        <div class="container">
+            <div class="text-content">
+                <h2>Manajemen Jadwal Ruang dan Guru FKIP</h2>
+                <p>Optimalkan, atur, dan akses penjadwalan ruang kelas dengan mudah.</p>
+                <div class="buttons">
+                    <a href="#" class="btn-signup">Sign Up</a>
+                    <a href="#" class="btn-login">Log In</a>
+                </div>
+            </div>
+            <div class="image-content">
+                <img src="<?php echo e(asset('images/foto1.jpg')); ?>" alt="Gambar Ruang Kelas dan Guru">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+<?php /**PATH C:\laragon\www\PPL-Project\resources\views/landingPage.blade.php ENDPATH**/ ?>
